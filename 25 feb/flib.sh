7ec7b9e4e4f4eb8256f6d225d026e752cd93781f
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-02-25 11:47:51 +0530

    Add files via upload

---
+++ 25 feb/flib.sh
@@ -0,0 +1,11 @@
+
+#invoke the to_lower()
+#to_lower "this is a test"
+function to_lower()
+{
+local str="$@"
+#echo " value of str is $str"
+#local output
+output=$(tr '[A-Z]' '[a-z]'<<<"${str}")
+echo $output
+}
