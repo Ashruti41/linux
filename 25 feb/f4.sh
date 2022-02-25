f56da273bf07e8dfcb62c9eb3e5a9256425f6251
Ashruti41 <98145165+Ashruti41@users.noreply.github.com>
2022-02-25 15:10:46 +0530

    Update f4.sh

--- 25 feb/f4.sh
+++ 25 feb/f4.sh
@@ -2,11 +2,10 @@
 #user found with function
 pass_file=/etc/passwd
 
+user(){
 read -p "enter the username " user
 grep "^$user" $pass_file>/dev/null
 status=$?
-user(){
-var=$1
 if test $status -eq 0
 then 
     echo "user exists"
@@ -14,4 +13,4 @@ else
     echo "user not found"
 fi
 }
-user $1
+user 
